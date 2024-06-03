c1bef48b067658a423376be6105e282448dcfaa8
Stina Terrazas <65819651+stinat@users.noreply.github.com>
2024-06-03 13:25:52 +0000

    from set up to cross-screen nav
    from set up to cross-screen nav ~Stína

--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+//import 'package:recipe_app/data/dummy_data.dart';
+import 'package:recipe_app/screens/categories.dart';
+//import 'package:recipe_app/screens/meals.dart';
+//import 'package:recipe_app/screens/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -22,6 +26,10 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
+      home: const CategoriesScreen(
+        //title: 'Some category...',
+        //meals: dummyMeals,
+      ),
 );
   }
 }
---
+++ recipe_app/lib/screens/categories.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import 'package:recipe_app/data/dummy_data.dart';
+
+import 'package:recipe_app/widgets/category_grid_item.dart';
+
+import 'package:recipe_app/screens/meals.dart';
+import 'package:recipe_app/models/category.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Choose Category'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          // availableCategories.map((category) => CategoryGridItem(category)).toList()
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
+        ],
+      ),
+    );
+  }
+}
