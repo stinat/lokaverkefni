36503f070356a675b68eb4f5b9945e09bd259b74
Stina Terrazas <65819651+stinat@users.noreply.github.com>
2024-06-05 19:55:19 +0000

    fav

--- recipe_app/lib/screens/tabs.dart
+++ recipe_app/lib/screens/tabs.dart
@@ -5,58 +5,82 @@ import 'package:recipe_app/screens/meals.dart';
 import 'package:recipe_app/screens/categories.dart';
 
 class TabsScreen extends StatefulWidget {
-const TabsScreen({super.key});
+  const TabsScreen({super.key});
 
-@override 
-State<TabsScreen> createState() {
-  return _TabsScreenState();
-}
-}
-
-class _TabsScreenState extends State<TabsScreen> {
-int _selectedPageIndex = 0;
-final List<Meal> _favoriteMeals = [];
-
-void _toggleMealFavoriteStatus(Meal meal) {
-  final isExisting = _favoriteMeals.contains(meal);
-
-  if (isExisting) {
-    _favoriteMeals.remove(meal);
-  } else {
-    _favoriteMeals.add(meal);
+  @override
+  State<TabsScreen> createState() {
+    return _TabsScreenState();
   }
 }
 
-void _selectPage(int index) {
-  setState(() {
-    _selectedPageIndex = index;
-  });
-}
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+  final List<Meal> _favoriteMeals = [];
 
-  @override 
-Widget build(BuildContext context) {
-Widget activePage = CategoriesScreen(onToggleFavorite: _toggleMealFavoriteStatus,);
-var activePageTitle = 'Categories';
+  void _showInfoMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message),
+    ),
+    );
+  }
 
-if (_selectedPageIndex == 1) {
-  activePage = MealsScreen(meals: [], onToggleFavorite: _toggleMealFavoriteStatus,);
-  activePageTitle = 'Your Favorites';
-}
+  void _toggleMealFavoriteStatus(Meal meal) {
+    final isExisting = _favoriteMeals.contains(meal);
 
+    if (isExisting) {
+      setState(() {
+        _favoriteMeals.remove(meal);
+      });
+      _showInfoMessage('Meal is no longer a favorite.');
+    } else {
+      setState(() {
+        _favoriteMeals.add(meal);
+        _showInfoMessage('Marked as a favorite!');
+      });
+    }
+  }
 
-  return Scaffold(
-  appBar: AppBar(
-    title: Text(activePageTitle),
-  ),
-  body: activePage,
-  bottomNavigationBar: BottomNavigationBar(
-    onTap: _selectPage,
-    currentIndex: _selectedPageIndex,
-  items: const [
-    BottomNavigationBarItem(icon: Icon(Icons.set_meal), label:'Categories', ),
-    BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorites', ),
-  ],
-  ),
-);
-}
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = CategoriesScreen(
+      onToggleFavorite: _toggleMealFavoriteStatus,
+    );
+    var activePageTitle = 'Categories';
+
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(
+        meals: _favoriteMeals,
+        onToggleFavorite: _toggleMealFavoriteStatus,
+      );
+      activePageTitle = 'Your Favorites';
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activePageTitle),
+      ),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
+  }
 }
